31c5a9985a8775b441f5d14c214b40d6a0200ad6
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2022-12-15 02:54:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Onevr
+
+Developed with Unreal Engine 4
---
+++ Source/Onevr/OnevrGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OnevrGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONEVR_API AOnevrGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
